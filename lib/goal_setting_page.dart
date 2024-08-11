bd91e098cd6eff0c5ab1fcfea35c7e6e2f48028b
Haruki1090 <haruharu.com.109607@gmail.com>
2024-08-11 11:13:10 +0900

    ゴール

---
+++ lib/goal_setting_page.dart
@@ -0,0 +1,39 @@
+import 'package:benesse_hackathon_2024_08/community_recommendation_page.dart';
+import 'package:benesse_hackathon_2024_08/selection_state.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class GoalSettingPage extends ConsumerWidget {
+  const GoalSettingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Set Your Final Goal'),
+      ),
+      body: Column(
+        children: [
+          DropdownButton<String>(
+            items: const [
+              DropdownMenuItem(value: '1 Month', child: Text('〜1ヶ月')),
+              DropdownMenuItem(value: '3 Months', child: Text('〜3ヶ月')),
+              DropdownMenuItem(value: '6 Months', child: Text('〜6ヶ月')),
+              DropdownMenuItem(value: '1 Year', child: Text('〜1年')),
+            ],
+            onChanged: (value) {
+              if (value != null) {
+                ref.read(selectionProvider.notifier).setGoal(value);
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (_) => const CommunityRecommendationPage()));
+              }
+            },
+          ),
+          // 適切な選択肢を追加
+        ],
+      ),
+    );
+  }
+}
