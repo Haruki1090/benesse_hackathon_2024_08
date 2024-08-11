d05295417bebca267a471fcdd801c14ba7c5a67e
Haruki1090 <haruharu.com.109607@gmail.com>
2024-08-11 11:43:16 +0900

    反映するデータの追加

--- lib/community_home_page.dart
+++ lib/community_home_page.dart
@@ -1,14 +1,46 @@
+// ignore_for_file: use_build_context_synchronously
+
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class CommunityHomePage extends StatelessWidget {
-  const CommunityHomePage({Key? key}) : super(key: key);
+  const CommunityHomePage({super.key});
+
+  Future<String?> _getCommunityName() async {
+    final user = FirebaseAuth.instance.currentUser;
+
+    if (user != null) {
+      final doc = await FirebaseFirestore.instance
+          .collection('users')
+          .doc(user.uid)
+          .get();
+
+      if (doc.exists) {
+        return doc.data()?['community'] as String?;
+      }
+    }
+    return null;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Community Name'),
+        title: FutureBuilder<String?>(
+          future: _getCommunityName(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Text('Loading...');
+            } else if (snapshot.hasError) {
+              return const Text('Error');
+            } else if (snapshot.hasData && snapshot.data != null) {
+              return Text(snapshot.data!);
+            } else {
+              return const Text('No Community');
+            }
+          },
+        ),
         actions: [
           IconButton(
             icon: const Icon(Icons.account_circle),
